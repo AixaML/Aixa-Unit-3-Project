15bb2e710c2d4ed0bf05d52ed14a2830a44551a0
AixaML <126012627+AixaML@users.noreply.github.com>
2023-02-21 10:23:26 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let firstButton=document.querySelector(".option-one");
+let secondButton=document.querySelector(".option-two");
+let thirdButton=document.querySelector(".option-three");
+
+let firstScreen=document.querySelector(".option-one-screen");
+let secondScreen=document.querySelector(".option-two-screen"); 
+let thirdScreen=document.querySelector(".option-three-screen"); 
+ 
+                                       firstButton.onclick=function(){
+    firstScreen.style.display="block";
+    firstScreen.style.display="block"; 
+     console.log("the end"); 
+ }; 
+
+secondButton.onclick=function(){
+   secondScreen.style.display="block";
+   secondScreen.style.display="block";
+     console.log("the end"); 
+ }; 
+
+thirdButton.onclick=function(){
+    thirdScreen.style.display="block"; 
+    thirdScreen.style.display="block"; 
+    console.log("the end");
+ };
+
+
+
